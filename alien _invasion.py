f50aa092ace9fabae57fe4b294c0551271bf8a98
Mr.Mysterio <mr.mysterio933@gmail.com>
2025-02-04 20:11:49 +0530

    use settings module

--- alien _invasion.py
+++ alien _invasion.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+from settings import Settings
 
 class AlienInvasion:
     """Overall class to manage game assets and behaviours"""
@@ -8,7 +9,9 @@ class AlienInvasion:
         """initialize atributes"""
         pygame.init()
         
-        self.screen = pygame.display.set_mode((1200,800))
+        self.settings = Settings()
+        self.clock = pygame.time.Clock()
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
     def run_game(self):
@@ -18,9 +21,11 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-                
+            
+                self.screen.fill(self.settings.bg_color)
                 # Make the most recently drawn screen visible.
                 pygame.display.flip()
+                self.clock.tick(60)
 
 if __name__ == '__main__':
     ai = AlienInvasion()
