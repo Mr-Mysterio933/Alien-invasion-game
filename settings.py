3d9ed7a8bc342ecc8fac718984b23254be2c0f42
Mr.Mysterio <mr.mysterio933@gmail.com>
2025-02-04 20:10:57 +0530

    create settings module for the game

---
+++ settings.py
@@ -0,0 +1,14 @@
+"""module that contains all the settings of the alien_invasion game"""
+
+class Settings:
+    """Contains all the settings"""
+
+    def __init__(self):
+        """intialize setting atributes"""
+
+        #screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (200,240,243)
+
+        
