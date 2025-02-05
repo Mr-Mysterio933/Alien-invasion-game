3f575806373b6cdb33745140382062b2459b9b0b
Mr.Mysterio <mr.mysterio933@gmail.com>
2025-02-05 15:26:57 +0530

    simplify the run_game method by adding helper methods

--- alien _invasion.py
+++ alien _invasion.py
@@ -21,15 +21,34 @@ class AlienInvasion:
         """start the main loop for the game"""
         while True:
             #watch for keyboard and mouse events
-            for event in pygame.event.get():
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+            self.clock.tick(60)
+
+    def _check_events(self):
+        """watch the keyboard and mouse events"""
+        for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-            
-                self.screen.fill(self.settings.bg_color)
-                self.ship.blitme()
-                # Make the most recently drawn screen visible.
-                pygame.display.flip()
-                self.clock.tick(60)
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RIGHT:
+                        #make the ship move to right
+                        self.ship.rect.x += 1
+                        self.ship.moving_right = True
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = False
+                
+                        
+
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
 
 if __name__ == '__main__':
     ai = AlienInvasion()
