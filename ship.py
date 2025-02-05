cd2db9737e4bf30902d70254c6b185bf0fd9e69a
Mr.Mysterio <mr.mysterio933@gmail.com>
2025-02-05 15:35:29 +0530

    making the ship move to left

--- ship.py
+++ ship.py
@@ -19,11 +19,15 @@ class Ship:
 
         # Movement flag ;starts with a ship that is not moving
         self.moving_right = False
+        self.moving_left = False
+
 
     def update(self):
         """Update the ship's position based on the movement of the flag"""
         if self.moving_right:
             self.rect.x += 1
+        if self.moving_left:
+            self.rect.x -= 1
 
     def blitme(self):
         """Draw the ship at its current location."""
